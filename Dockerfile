a1cf3a93b920e103bbcb8908749bf67649ef014f
Davis Voicescuks <davis.voicescuks@gmail.com>
2017-05-31 23:11:05 +0300

    Use node container.

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,8 @@
-FROM nginx:alpine
-COPY build /usr/share/nginx/html
+FROM node:7.10.0-alpine
+ENV NODE_ENV=production
+RUN mkdir -p /usr/src/wowanalyzer
+COPY . /usr/src/wowanalyzer
+WORKDIR /usr/src/wowanalyzer
+RUN npm install
+USER node
+CMD [ "node", "server/index.js" ]
---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const staticFiles = express.static(path.join(__dirname, '../build'));
+
+app.use(staticFiles);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
+
+app.listen(8080);
